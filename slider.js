54fc1f719da2b71785ad294737ddb07e83887995
SamCares <44313759+SamCares@users.noreply.github.com>
2022-06-23 22:13:20 +0100

    testing js

--- slider.js
+++ slider.js
@@ -2,3 +2,12 @@ alert("Hello Slider");
 
 const prevButton = document.querySelector("prev");
 const nextButton = document.querySelector("next");
+
+// function slideTestimonialLeft() {
+
+// }
+
+prevButton.addEventListener('click', () => {
+    console.log(`This was clicked ${slideTestimonialLeft()}`);
+})
+
